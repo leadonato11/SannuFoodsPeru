a721194a1b5d64976310206fc868e2237c861f06
leadonato11 <leacavs11@gmail.com>
2021-07-26 00:20:25 -0300

    Finalización de script de contacto

--- js/email.js
+++ js/email.js
@@ -1,21 +1,38 @@
+const form = document.getElementById("form");
+const successSection = document.getElementById("successSection");
+const messageSent = document.getElementById("successMessage");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+window.onload = function () {
+  $("#successSection").hide();
+};
+
 (function () {
   emailjs.init("user_6RkM9Tahr3OkRQtLACcpW");
 })();
-window.onload = function () {
-  document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault();
 
-    emailjs.sendForm("service_y5bvfmt", "template_vc7u9mi", this).then(
-      function () {
-        window.location.href = "#";
-        console.log("Mensaje Enviado");
-      },
-      function (error) {
-        console.log("No se pudo enviar el Email", error);
-      }
-    );
-  });
-};
-/* Instalar emailjs con npm npm install emailjs-com --save */
-/* Agregar name en los input con full-name, email y message */
-/* <input class="form-control" type="hidden" name="subject" value="Sannu Foods"> */
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (successSection.style.display === "none") {
+    $("#successSection").show();
+  }
+  nombre.classList.add("inputs--disable");
+  email.classList.add("inputs--disable");
+  message.classList.add("inputs--disable");
+  btnSubmit.classList.add("btnSannu--disable");
+  messageSent.innerHTML = "¡Mensaje enviado!";
+
+  emailjs.sendForm("service_y5bvfmt", "template_vc7u9mi", this).then(
+    function () {
+      window.location.href = "#";
+      console.log("Mensaje Enviado");
+    },
+    function (error) {
+      console.log("No se pudo enviar el Email", error);
+    }
+  );
+  form.reset();
+});
